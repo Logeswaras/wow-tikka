de5ca352d3126a22990a664a8fc599534e30178a
movin-yectra <movin@yectra.com>
2023-09-21 17:01:15 +0530

    fix routing

--- src/pages/BaseLayout.tsx
+++ src/pages/BaseLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../common/components/layout/NavBar";
+import StickyHeader from "../common/components/layout/StickyHeader";
 
 const BaseLayout = () => {
   return (
     <>
       <div className="">
+        <StickyHeader />
         <NavBar />
       </div>
       <Outlet />
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,50 @@
-import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
+import Banner from "./components/Banner";
+import OrderOnline from "./components/OrderOnline";
+import Card from "../../common/ui/Card";
 import SearchBar from "../../common/components/layout/SearchBar";
-import StickyHeader from "../../common/components/layout/StickyHeader";
+
+interface ProductData {
+  image: string;
+  title: string;
+}
 
 const Home = () => {
+  const [cardData, setCardData] = useState<Array<ProductData>>([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:9000/products")
+      .then((res) => setCardData(res.data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
   return (
-    <div className="mb-5">
-      <StickyHeader />
-      <SearchBar />
+    <>
       <div>
-        <Outlet />
+        <SearchBar />
+        <Banner />
       </div>
-    </div>
+      <div className="px-5">
+        <OrderOnline />
+      </div>
+      <div className="px-5">
+        <div className="mt-5">
+          <div className="row">
+            {cardData &&
+              cardData.map((card, i) => (
+                <div
+                  className="col-12 col-xl-4 col-lg-4 col-md-6 col-sm-6"
+                  key={i}
+                >
+                  <Card image={card.image} title={card.title} />
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
